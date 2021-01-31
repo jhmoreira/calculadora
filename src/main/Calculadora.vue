dbb6c220d3171c48955cc4597994027eabc19abf
jhmoreira <joaomoreirati98@gmail.com>
2021-01-31 10:08:04 -0300

    Modificando component  button

--- src/main/Calculadora.vue
+++ src/main/Calculadora.vue
@@ -1,27 +1,43 @@
 <template>
   <div class="calculadora">
-      <Display/>
-      <Button/>
+    <Display />
+    <Button label="AC" triple />
+    <Button label="/" operation />
+    <Button label="7" />
+    <Button label="8" />
+    <Button label="9" />
+    <Button label="*" operation />
+    <Button label="4" />
+    <Button label="5" />
+    <Button label="6" />
+    <Button label="-" operation />
+    <Button label="1" />
+    <Button label="2" />
+    <Button label="3" />
+    <Button label="+" operation />
+    <Button label="0" double />
+    <Button label="." />
+    <Button label="=" operation />
   </div>
 </template>
 
 <script>
-import Display from "../components/Display"
-import Button from "../components/Button"
+import Display from "../components/Display";
+import Button from "../components/Button";
 export default {
-    components:{Button,Display}
-}
+  components: { Button, Display },
+};
 </script>
 
 <style>
-.calculadora{
-    height: 320px ;
-    width: 235px;
-    border-radius: 5px;
-    overflow: hidden;
+.calculadora {
+  height: 320px;
+  width: 235px;
+  border-radius: 5px;
+  overflow: hidden;
 
-    display: grid;
-    grid-template-columns: repeat(4 , 25%);
-    grid-template-rows: 1fr 48px 48px 48px 48px 48px;
+  display: grid;
+  grid-template-columns: repeat(4, 25%);
+  grid-template-rows: 1fr 48px 48px 48px 48px 48px;
 }
 </style>
